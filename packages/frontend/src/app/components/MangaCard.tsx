e1fed0e1afad9d93673e052545b0bae246a6c466
kirik-streltsov <kirik.streltsov0@yandex.ru>
2025-09-14 13:57:21 +0300

    Add fallback image

--- packages/frontend/src/app/components/MangaCard.tsx
+++ packages/frontend/src/app/components/MangaCard.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 interface MangaCardProps {
   title: string;
-  thumbnailUrl: string;
+  thumbnailUrl?: string;
 }
 
 function MangaCard({ title, thumbnailUrl }: MangaCardProps) {
   return (
     <figure className="flex flex-col relative w-35 h-50 rounded-sm overflow-hidden">
-      <Image src={thumbnailUrl} alt={title} fill />
+      <Image src={thumbnailUrl ?? "/logo-white.jpg"} alt={title} fill />
       <figcaption
         className="text-white mt-44 z-10 text-center"
         style={{
