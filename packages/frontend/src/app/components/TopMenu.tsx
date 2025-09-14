dce0c4f68584987b035d7f34e4e999d7aba23771
kirik-streltsov <kirik.streltsov0@yandex.ru>
2025-09-14 12:11:25 +0300

    Redesign top menu

--- packages/frontend/src/app/components/TopMenu.tsx
+++ packages/frontend/src/app/components/TopMenu.tsx
@@ -1,32 +1,35 @@
-import Image from "next/image";
 import Link from "next/link";
 
 function TopMenu() {
   return (
     <nav>
-      <ul className="flex gap-5 content-end items-center bg-white min-h-15 px-20">
+      <ul className="flex gap-5 content-end items-center bg-light-grey min-h-15 px-20">
         <li className="mr-auto">
           <Link className="flex items-center gap-2" href="/">
-            <Image
-              src="/logo.png"
-              alt="Mangadrill Logo"
-              width="60"
-              height="60"
-            />
-            <span className="text-xl font-bold">Mangadrill</span>
+            <span className="text-xl font-bold text-white hover:text-bright-red">
+              Mangadrill
+            </span>
           </Link>
         </li>
         <li>
-          <Link href="/">Главная</Link>
+          <Link href="/" className="text-white hover:text-bright-red">
+            Главная
+          </Link>
         </li>
         <li>
-          <Link href="/genres">Жанры</Link>
+          <Link href="/genres" className="text-white hover:text-bright-red">
+            Жанры
+          </Link>
         </li>
         <li>
-          <Link href="/about">О нас</Link>
+          <Link href="/about" className="text-white hover:text-bright-red">
+            О нас
+          </Link>
         </li>
         <li>
-          <Link href="/login">Войти</Link>
+          <Link href="/login" className="text-white hover:text-bright-red">
+            Войти
+          </Link>
         </li>
       </ul>
     </nav>
